10be96f5eb3bab29819e6040a31732c5323fd874
2180606 <47970657+2180606@users.noreply.github.com>
2019-05-14 16:30:13 +0100

    Add Images e start do layout

--- ProjetoDa/ProjetoDa/Form1.Designer.cs
+++ ProjetoDa/ProjetoDa/Form1.Designer.cs
@@ -28,73 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonGestaoClientes = new System.Windows.Forms.Button();
+            this.buttonGestaoOficina = new System.Windows.Forms.Button();
+            this.buttonGestaoVendas = new System.Windows.Forms.Button();
+            this.buttonGestaoAluguer = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // listBox1
+            // buttonGestaoClientes
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(423, 289);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(244, 108);
-            this.listBox1.TabIndex = 11;
+            this.buttonGestaoClientes.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.buttonGestaoClientes.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonGestaoClientes.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonGestaoClientes.Image = ((System.Drawing.Image)(resources.GetObject("buttonGestaoClientes.Image")));
+            this.buttonGestaoClientes.Location = new System.Drawing.Point(35, 71);
+            this.buttonGestaoClientes.Name = "buttonGestaoClientes";
+            this.buttonGestaoClientes.Size = new System.Drawing.Size(200, 200);
+            this.buttonGestaoClientes.TabIndex = 0;
+            this.buttonGestaoClientes.Text = "Gestão Clientes";
+            this.buttonGestaoClientes.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonGestaoClientes.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // buttonGestaoOficina
             // 
-            this.button1.Location = new System.Drawing.Point(205, 304);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 10;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            this.buttonGestaoOficina.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.buttonGestaoOficina.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonGestaoOficina.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonGestaoOficina.Image = ((System.Drawing.Image)(resources.GetObject("buttonGestaoOficina.Image")));
+            this.buttonGestaoOficina.Location = new System.Drawing.Point(295, 71);
+            this.buttonGestaoOficina.Name = "buttonGestaoOficina";
+            this.buttonGestaoOficina.Size = new System.Drawing.Size(200, 200);
+            this.buttonGestaoOficina.TabIndex = 0;
+            this.buttonGestaoOficina.Text = "Gestão Oficina";
+            this.buttonGestaoOficina.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonGestaoOficina.UseVisualStyleBackColor = true;
             // 
-            // textBox4
+            // buttonGestaoVendas
             // 
-            this.textBox4.Location = new System.Drawing.Point(185, 226);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(284, 20);
-            this.textBox4.TabIndex = 9;
+            this.buttonGestaoVendas.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.buttonGestaoVendas.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonGestaoVendas.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonGestaoVendas.Image = ((System.Drawing.Image)(resources.GetObject("buttonGestaoVendas.Image")));
+            this.buttonGestaoVendas.Location = new System.Drawing.Point(35, 302);
+            this.buttonGestaoVendas.Name = "buttonGestaoVendas";
+            this.buttonGestaoVendas.Size = new System.Drawing.Size(200, 200);
+            this.buttonGestaoVendas.TabIndex = 0;
+            this.buttonGestaoVendas.Text = "Gestão Vendas";
+            this.buttonGestaoVendas.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonGestaoVendas.UseVisualStyleBackColor = true;
             // 
-            // textBox3
+            // buttonGestaoAluguer
             // 
-            this.textBox3.Location = new System.Drawing.Point(157, 158);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(240, 20);
-            this.textBox3.TabIndex = 8;
+            this.buttonGestaoAluguer.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.buttonGestaoAluguer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonGestaoAluguer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonGestaoAluguer.Image = ((System.Drawing.Image)(resources.GetObject("buttonGestaoAluguer.Image")));
+            this.buttonGestaoAluguer.Location = new System.Drawing.Point(295, 302);
+            this.buttonGestaoAluguer.Name = "buttonGestaoAluguer";
+            this.buttonGestaoAluguer.Size = new System.Drawing.Size(200, 200);
+            this.buttonGestaoAluguer.TabIndex = 0;
+            this.buttonGestaoAluguer.Text = "Gestão Aluguer";
+            this.buttonGestaoAluguer.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonGestaoAluguer.UseVisualStyleBackColor = true;
             // 
-            // textBox2
+            // label1
             // 
-            this.textBox2.Location = new System.Drawing.Point(133, 103);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(276, 20);
-            this.textBox2.TabIndex = 7;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(157, 53);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(197, 20);
-            this.textBox1.TabIndex = 6;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Escolha uma funcionalidade:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(529, 535);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonGestaoAluguer);
+            this.Controls.Add(this.buttonGestaoVendas);
+            this.Controls.Add(this.buttonGestaoOficina);
+            this.Controls.Add(this.buttonGestaoClientes);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Stand Automóvel";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -102,12 +121,11 @@
 
         #endregion
 
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonGestaoClientes;
+        private System.Windows.Forms.Button buttonGestaoOficina;
+        private System.Windows.Forms.Button buttonGestaoVendas;
+        private System.Windows.Forms.Button buttonGestaoAluguer;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- ProjetoDa/ProjetoDa/Form1.cs
+++ ProjetoDa/ProjetoDa/Form1.cs
@@ -19,18 +19,5 @@ namespace ProjetoDa
             container = new BaseDadosContainer();
         }
 
-        private void Button1_Click(object sender, EventArgs e)
-        {
-            Cliente tempCliente = new Cliente();
-            Cliente clienteSelecionado = new Cliente();
-            int.TryParse(textBox2.Text, out int tempNIF);
-            tempCliente.Nome = textBox1.Text; //Nome Cliente
-            tempCliente.NIF = tempNIF;// NIF Cliente
-            tempCliente.Morada = textBox3.Text; // Morada Cliente
-            tempCliente.Contacto = textBox4.Text;// Contacto Cliente
-            container.Clientes.Add(tempCliente);
-            container.SaveChanges();
-
-        }
     }
 }
