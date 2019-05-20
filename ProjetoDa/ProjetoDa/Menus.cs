3ba53cea3fc3c1cf96a054810d30b6243eaba1dd
2180606 <47970657+2180606@users.noreply.github.com>
2019-05-20 09:01:51 +0100

    minor changes g.cliente

--- ProjetoDa/ProjetoDa/Menus.cs
+++ ProjetoDa/ProjetoDa/Menus.cs
@@ -14,6 +14,7 @@ namespace ProjetoDa
     {
         private BaseDadosContainer container;
         GestaoClientes gestaoClientes = new GestaoClientes();
+        
         public Menus()
         {
             InitializeComponent();
