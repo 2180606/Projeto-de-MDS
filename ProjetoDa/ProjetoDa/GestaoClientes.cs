bb870c74dcd8770e93fe698ca49a5b9880d06543
2180606 <47970657+2180606@users.noreply.github.com>
2019-05-27 10:33:22 +0100

    Revert "commit diogo - clientes"
    This reverts commit 3b5b9b5d6d4a6ec0b9b7045a7694f8b4ca9b9e73.

--- ProjetoDa/ProjetoDa/GestaoClientes.cs
+++ ProjetoDa/ProjetoDa/GestaoClientes.cs
@@ -19,16 +19,12 @@ namespace ProjetoDa
             container = new BaseDadosContainer();
         }
 
-        int selectedRow;
-
         private void GestaoClientes_Load(object sender, EventArgs e)
         {
             // TODO: This line of code loads data into the 'baseDadosDADataSet.Clientes' table. You can move, or remove it, as needed.
             this.clientesTableAdapter.Fill(this.baseDadosDADataSet.Clientes);
-
-            textBoxNomeCliente.MaxLength = 9;
-
         }
+
 
 
 
@@ -81,113 +77,6 @@ namespace ProjetoDa
             MessageBox.Show("alteracao feita com sucesso", "guardado", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
-        private void ClientesBindingNavigatorSaveItem_Click(object sender, EventArgs e)
-        {
-            this.clientesTableAdapter.Fill(this.baseDadosDADataSet.Clientes);
-
-
-
-            //this.Validate();
-            //this.clientesBindingSource.EndEdit();
-            //this.tableAdapterManager.UpdateAll(this.baseDadosDADataSet);
-
-        }
-
-        private void BindingNavigatorAddNewItem_Click(object sender, EventArgs e)
-        {
-            //converter string to int
-            int MyInteger = Convert.ToInt32(TextBoxNifCliente.Text);
-
-            Cliente tempcliente = new Cliente();
-            
-            //verificacoes
-            if (string.IsNullOrWhiteSpace(textBoxNomeCliente.Text))
-            {
-                // Meter label para servir de Mensagem de Erros de Preenchimento
-                textBoxNomeCliente.Select();
-                return;
-            }
-            if (string.IsNullOrWhiteSpace(TextBoxNifCliente.Text))
-            {
-                // Meter label para servir de Mensagem de Erros de Preenchimento
-                TextBoxNifCliente.Select();
-                return;
-            }
-            if (string.IsNullOrWhiteSpace(textBoxContactoCliente.Text))
-            {
-                // Meter label para servir de Mensagem de Erros de Preenchimento
-                textBoxContactoCliente.Select();
-                return;
-            }
-            if (string.IsNullOrWhiteSpace(textBoxMoradaCliente.Text))
-            {
-                // Meter label para servir de Mensagem de Erros de Preenchimento
-                textBoxMoradaCliente.Select();
-                return;
-            }
-
-            foreach (Cliente cliente in container.Clientes.ToList<Cliente>())
-            {
-                if (MyInteger == cliente.NIF)
-                {
-                    MessageBox.Show("O Cliente com o respetivo NIF ja se encontra inserido na base de dados");
-                    TextBoxNifCliente.Select();
-                    return;
-                }
-            }
-            
-            tempcliente.Nome = textBoxNomeCliente.Text;
-            tempcliente.NIF = MyInteger;
-            tempcliente.Morada = textBoxMoradaCliente.Text;
-            tempcliente.Contacto = textBoxContactoCliente.Text;
-
-
-            container.Clientes.Add(tempcliente);
-
-            textBoxNomeCliente.Clear();
-            TextBoxNifCliente.Clear();
-            textBoxContactoCliente.Clear();
-            textBoxMoradaCliente.Clear();
-      
-
-            container.SaveChanges();
-            this.DialogResult = DialogResult.OK;
-            //this.Close();
-        }
-
-        private void BindingNavigatorDeleteItem_Click_1(object sender, EventArgs e)
-        {
-
-            // delete datagridview row selected row
-            selectedRow = clientesDataGridView.CurrentCell.RowIndex;
-            clientesDataGridView.Rows.RemoveAt(selectedRow);
-
-        }
-
-
-        private void ClientesDataGridView_CellClick(object sender, DataGridViewCellEventArgs e)
-        {
-            DataGridViewRow row = this.clientesDataGridView.Rows[e.RowIndex];
-
-
-
-            if (e.RowIndex >= 0)
-            {
-                //gets a collection that contains all the rows
-                //populate the textbox from specific value of the coordinates of column and row.
-                textBoxNomeCliente.Text = row.Cells[1].Value.ToString();
-                TextBoxNifCliente.Text = row.Cells[2].Value.ToString();
-                textBoxContactoCliente.Text = row.Cells[3].Value.ToString();
-                textBoxMoradaCliente.Text = row.Cells[4].Value.ToString();
-
-            }
-
-        }
-
-        private void Button2_Click(object sender, EventArgs e)
-        {
-            selectedRow = clientesDataGridView.CurrentCell.RowIndex;
-            clientesDataGridView.Rows.RemoveAt(selectedRow);
-        }
+       
     }
 }
--- ProjetoDa/ProjetoDa/GestaoVendas.cs
+++ ProjetoDa/ProjetoDa/GestaoVendas.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ProjetoDa
@@ -23,32 +17,75 @@ namespace ProjetoDa
         private void ButtonAdicionarCarroVenda_Click(object sender, EventArgs e)
         {
             DialogResult dialogResult = adicionarCarroVenda.ShowDialog(this);
-            if ( dialogResult == DialogResult.OK || dialogResult == DialogResult.Cancel)
+            if (dialogResult == DialogResult.OK || dialogResult == DialogResult.Cancel)
             {
-                atualizarListBoxes();
+                AtualizarListBoxes();
             }
         }
-        private void atualizarListBoxes()
+        private void AtualizarListBoxes()
         {
+            Cliente clienteSelecionado = new Cliente();
+            clienteSelecionado = (Cliente)listBoxClientes.SelectedItem;
             listBoxCarros.DataSource = null;
             listBoxClientes.DataSource = null;
+            listBoxVendasEfetuadas.DataSource = null;
             listBoxClientes.DataSource = container.Clientes.ToList<Cliente>();
             listBoxCarros.DataSource = container.Carros.OfType<CarroVenda>().ToList<Carro>();
-
+            if(clienteSelecionado!=null)
+                listBoxVendasEfetuadas.DataSource = clienteSelecionado.Vendas.ToList<Venda>();
         }
         private void ButtonEliminarCarroVenda_Click(object sender, EventArgs e)
         {
             DialogResult dialogResult = eliminarCarroVenda.ShowDialog(this); ;
             if (dialogResult == DialogResult.OK || dialogResult == DialogResult.Cancel)
             {
-                atualizarListBoxes();
+                AtualizarListBoxes();
             }
-            
+
         }
 
         private void GestaoVendas_Load(object sender, EventArgs e)
         {
-            atualizarListBoxes();
+            AtualizarListBoxes();
+        }
+
+        private void ListBoxCarros_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CarroVenda carroSelecionado;
+            if (listBoxCarros.SelectedIndex != -1)
+            {
+                carroSelecionado = (CarroVenda)listBoxCarros.SelectedItem;
+                labelChassisCarro.Text = carroSelecionado.NumeroChassis;
+                labelCombustivelCarro.Text = carroSelecionado.Combustivel;
+                labelExtrasCarro.Text = carroSelecionado.Extras;
+                labelMarcaCarro.Text = carroSelecionado.Marca;
+                labelModeloCarro.Text = carroSelecionado.Modelo;
+            }
+
+        }
+
+        private void ButtonVenderCarro_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult;
+
+
+            if (listBoxClientes.SelectedIndex != -1)
+            {
+                Cliente clienteSelecionado = (Cliente)listBoxClientes.SelectedItem;
+                VenderCarro venderCarro = new VenderCarro(clienteSelecionado);
+
+                dialogResult = venderCarro.ShowDialog();
+
+            }
+            else
+            {
+                dialogResult = DialogResult.Abort;
+                MessageBox.Show("Escolha o Cliente ao qual pretende Vender um Carro.");
+            }
+            if ( dialogResult == DialogResult.Cancel || dialogResult == DialogResult.OK )
+            {
+                AtualizarListBoxes();
+            }
         }
     }
 }
